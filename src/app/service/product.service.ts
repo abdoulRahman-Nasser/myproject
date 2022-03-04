6837a0653af58adf961ad842b69807c110f6d822
abdoulRahman-Nasser <Abdoulrahman.nasser.zarzora@gmail.com>
2022-03-05 00:23:50 +0200

    final2

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -10,9 +10,9 @@ export class ProductServiceService {
   productList =
     [
       { ID: 1, Name: 'Samsung', Quantity: 1, Price: 1500, Img: 'assets/galaxy a12.jpg' },
-      { ID: 2, Name: 'xiaomi', Quantity: 1, Price: 1000, Img: 'assets/microsoft surface.jpg' },
+      { ID: 2, Name: 'xiaomi', Quantity: 1, Price: 1000, Img: 'assets/microsoft surface .jpg' },
       { ID: 3, Name: 'Microsoft', Quantity: 1, Price: 2000, Img: 'assets/xiaomi 11 lite.jpg' },
-      { ID: 3, Name: 'apple', Quantity: 1, Price: 20000, Img: 'iphone 12 pro max.jpg' },
+      { ID: 3, Name: 'apple', Quantity: 1, Price: 20000, Img: 'assets/iphone 12 pro max.jpg' },
     ]
   productlistbyID={}
   constructor() {
